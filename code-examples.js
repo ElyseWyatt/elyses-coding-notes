20db3bd28df224ac223fda59af8d767e5412b843
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2017-12-03 18:23:01 +1300

    Edits.

--- code-examples.js
+++ code-examples.js
@@ -57,3 +57,24 @@ for (var y = 0; y < size; y++) {
 }
 
 console.log(board);
+
+
+
+
+//Minesweeper.
+
+// In startGame, above lib.initBoard(), write a for loop. This should loop through //the contents of board.cells. (Remember, board.cells is an array of objects.) The //loop's only job should be to call countSurroundingMines once for each cell in //board.cells. You'll need to pass each cell as an argument (the bit in the //parentheses). Assign the result of countSurroundingMines to a property on each cell //object. The new property should be called surroundingMines.
+//
+//In startGame, use document.addEventListener to call checkForWin every time the left //mouse button is clicked. Add another one that calls checkForWin when the right //mouse button is clicked.
+//
+
+function startGame () {
+  for (let i = 0; i < board.cells.length; i++ ) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  }
+
+document.addEventListener('click', checkForWin)
+document.addEventListener('contextmenu', checkForWin)
+
+  lib.initBoard()
+}
