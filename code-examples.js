19b7f3c0ece5c9227a620d4e4ad08c0bde9806e3
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2017-12-03 17:48:28 +1300

    Edits.

---
+++ code-examples.js
@@ -0,0 +1,13 @@
+//Looping A Triangle Exercise.
+
+//Write a loop that looks like this:
+//#
+//##
+//###
+//####
+//#####
+//######
+//#######
+
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
