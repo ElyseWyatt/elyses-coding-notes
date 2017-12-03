75906b02d16a6478bff13375131c15e001e77649
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2017-12-04 11:15:21 +1300

    Edits.

--- code-examples.js
+++ code-examples.js
@@ -78,3 +78,21 @@ document.addEventListener('contextmenu', checkForWin)
 
   lib.initBoard()
 }
+
+
+//Filtering out parts of an array.
+
+var animals = [
+{ name: 'Fluffykins', species: 'rabbit'},
+{ name: 'Caro',       species: 'dog'},
+{ name: 'Hamilton',   species: 'dog'},
+{ name: 'Harold',     species: 'fish'},
+{ name: 'Ursula',     species: 'cat'},
+{ name: 'Jimmy',      species: 'fish'}
+]
+
+var dogs []
+for (var i = 0; i < animals.length; i++) {
+  if (animals[i].species === 'dog')
+  dogs.push(animals[i])
+}
