96b0923fcb25d8564e393988ae8b309e740c733b
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2017-12-04 15:46:59 +1300

    Edits.

--- code-examples.js
+++ code-examples.js
@@ -80,7 +80,7 @@ document.addEventListener('contextmenu', checkForWin)
 }
 
 
-//Filtering out parts of an array.
+//Filtering out parts of an array. In this instance, we want to filter out any instance of Dog from the array.
 
 var animals = [
 { name: 'Fluffykins', species: 'rabbit'},
@@ -96,3 +96,117 @@ for (var i = 0; i < animals.length; i++) {
   if (animals[i].species === 'dog')
   dogs.push(animals[i])
 }
+
+
+//Rewriting using Filter function. This is a much shorter and faster way of writing the code, and gets the same outcome. The Filter function loops through each array.
+
+var dogs = animals.filter(function(animal) {
+  return animal.species === 'dog'
+})
+
+//Want to just log all the names of the animals? This loops the animals array and pushes it into the names array.
+
+var animals = [
+{ name: 'Fluffykins', species: 'rabbit'},
+{ name: 'Caro',       species: 'dog'},
+{ name: 'Hamilton',   species: 'dog'},
+{ name: 'Harold',     species: 'fish'},
+{ name: 'Ursula',     species: 'cat'},
+{ name: 'Jimmy',      species: 'fish'}
+]
+
+var names = []
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name)
+}
+
+//We are going to do the same thing with Map instead. It will log the same thing as above.
+
+var names = animals.map(function(animal) {
+  return animal.name
+})
+
+//Lets now display the entire sentence in each line of our array! This will print the names ans species of each animal in a sentence.
+
+var names = animals.map(function(animal) {
+  return animal.name + ' is a ' + animal.species
+})
+
+//Let's make it even shorter with this:
+
+var names = animals.map((animal) => animal.name)
+
+//Aaaaaannd, even shorter again!
+
+var names = animals.map((x) => x.name)
+
+
+//Using Reduce function. The mission is to summarise all //the amounts in the array. We'll end up with a total //amount.
+
+var orders = [
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 }
+]
+
+var totalAmount = 0
+for (var i = 0; i < orders.length; i++ ) {
+  totalAmount += orders[i].amount
+}
+
+//We can get the same thing using this:
+
+var totalAmount = orders.reduce(function(sum, order){
+  return sum + order.amount
+}, 0)
+
+//Let's do the same, but with arrow functions
+
+var totalAmount = orders.reduce((sum, order) => sum + order.amount, 0)
+
+
+///Free Code Camp
+
+//In the editor, use the Car constructor to create a new instance and assign it to myCar.
+
+//Then give myCar a nickname property with a string value.
+
+var myCar = new Car();
+myCar.nickname = "Tommy";
+
+
+
+
+///Reverse a string using this initial code:
+
+function reverseString(str) {
+ return joinArray;
+}
+reverseString("hello");
+
+//First, the string needs to be split into an array:
+
+ var splitString = str.split("");
+
+ //Then, it can be reversed:
+
+  var reverseArray = splitString.reverse();
+
+  //Then, it needs to be joined back together:
+
+  var joinArray = reverseArray.join("");
+
+  //Here is the final code all laid out:
+
+
+  function reverseString(str) {
+
+  var splitString = str.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+
+  return joinArray;
+}
+
+reverseString("hello");
