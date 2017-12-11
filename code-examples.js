d75e066841334bc7f50d3e9753c8d1bd70069609
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2017-12-11 16:12:27 +1300

    Edits.

--- code-examples.js
+++ code-examples.js
@@ -247,3 +247,97 @@ console.log("My" + shortArray);
 
 let longArray = myArray.filter(word => word.length <= 5);
 console.log("My" + longArray);
+
+
+// 1. Functions: Write a function min that takes two arguments and returns their //minimum.
+
+//My answer:
+
+var min = function() {
+  return
+}
+
+console.log(Math.min(0, 10));
+// → 0
+console.log(Math.min(0, -10));
+// → -10
+
+
+//Their answer:
+
+function min(a, b) {
+  if (a < b)
+    return a;
+  else
+    return b;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+
+//2. Recursion
+
+*// We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+
+ *//Zero is even.
+
+ //One is odd.
+
+ //For any other number N, its evenness is the same as N - 2.
+
+/*Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
+
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?*/
+
+
+//Starting code:
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+
+/*Your function will likely look somewhat similar to the inner find function in the recursive findSolution example in this chapter, with an if/else if/else chain that tests which of the three cases applies. The final else, corresponding to the third case, makes the recursive call. Each of the branches should contain a return statement or in some other way arrange for a specific value to be returned.
+
+When given a negative number, the function will recurse again and again, passing itself an ever more negative number, thus getting further and further away from returning a result. It will eventually run out of stack space and abort.*/
+
+
+//Finished code:
+
+function isEven(n) {
+    if (n == 0)
+      return true;
+    else if (n == 1)
+      return false;
+    else if (n < 0)
+      return isEven(-n);
+    else
+      return isEven(n - 2);
+}
+
+
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+
+
+//Notes:
+//Very basic If statement:
+
+function thisOne (x) {
+  if (x < 2)
+  return "Yes";
+  else
+  return "No";
+}
+
+console.log(thisOne(1))
+//Yes
